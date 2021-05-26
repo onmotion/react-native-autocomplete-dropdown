92a275424459c359ccfbfbb006157ef24e443590
Alexandr Kozhevnikov <onmotion1@gmail.com>
2021-05-26 11:57:33 +0300

    add typings

---
+++ src/index.d.ts
@@ -0,0 +1,43 @@
+import React, { FC } from 'react'
+import { StyleProp, ViewStyle } from 'react-native'
+
+export type TAutocompleteDropdownItem = {
+  id: string
+  title: string | null
+}
+
+interface AutocompleteDropdownProps {
+  /**
+   * @example [
+   *     { id: "1", title: "Alpha" },
+   *     { id: "2", title: "Beta" },
+   *     { id: "3", title: "Gamma" }
+   * ]
+   */
+  dataSet?: TAutocompleteDropdownItem[]
+  suggestionsListMaxHeight?: number,
+  initialValue?: string | object,
+  loading?: boolean,
+  useFilter?: boolean,
+  showClear?: boolean,
+  showChevron?: boolean,
+  closeOnBlur?: boolean,
+  clearOnFocus?: boolean,
+  debounce?: number,
+  bottomOffset?: number,
+  onChangeText?(text: string): any,
+  onSelectItem?(item: TAutocompleteDropdownItem): any,
+  onOpenSuggestionsList?(isOpened: boolean): any,
+  onClear?(): any,
+  onSubmit?(e: any): any,
+  onFocus?(e: any): any,
+  controller?(controller: any): any,
+  containerStyle?: StyleProp<ViewStyle>,
+  rightButtonsContainerStyle?: StyleProp<ViewStyle>,
+  suggestionsListContainerStyle?: StyleProp<ViewStyle>,
+  ChevronIconComponent?: JSX.Element,
+  ClearIconComponent?: JSX.Element,
+  ScrollViewComponent?: JSX.Element,
+}
+
+export const AutocompleteDropdown: FC<AutocompleteDropdownProps>
