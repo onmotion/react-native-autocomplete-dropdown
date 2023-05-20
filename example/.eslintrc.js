0e756c2164d785f2aa7e672407102381c209c1b1
Alexandr Kozhevnikov <onmotion1@gmail.com>
2023-05-20 08:13:05 +0200

    upgrade packages

---
+++ example/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', 'plugin:prettier/recommended', 'prettier'],
+  plugins: ['react', 'unused-imports', 'prettier'],
+  rules: {
+    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 110,
+        arrowParens: 'avoid',
+        bracketSameLine: true,
+        bracketSpacing: true,
+        singleQuote: true,
+        trailingComma: 'none',
+        tabWidth: 2,
+        useTabs: false,
+        semi: false
+      }
+    ],
+    semi: 'off',
+    'comma-dangle': ['warn', 'never'],
+    'react-native/no-inline-styles': 'off',
+    'react-hooks/exhaustive-deps': 'off'
+  }
+}
