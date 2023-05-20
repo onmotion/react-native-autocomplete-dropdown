9b87efe213b8de34eb95ddb8173ac8dbdd889fe8
Alexandr Kozhevnikov <onmotion1@gmail.com>
2023-05-20 08:13:05 +0200

    upgrade packages

--- example/ios/AutocomleteDropdownPlaygroundTests/AutocomleteDropdownPlaygroundTests.m
+++ example/ios/AutocomleteDropdownPlaygroundTests/AutocomleteDropdownPlaygroundTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectForAutocomleteDropdownTests : XCTestCase
+@interface AutocomleteDropdownPlaygroundTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectForAutocomleteDropdownTests
+@implementation AutocomleteDropdownPlaygroundTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
