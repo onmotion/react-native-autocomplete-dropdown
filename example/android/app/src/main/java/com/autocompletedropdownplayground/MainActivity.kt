ce0b20a8ac889d384a164c71cc780752ea0255dc
Alexandr Kozhevnikov <onmotion1@gmail.com>
2025-05-07 15:27:51 +0200

    fix: typo

--- example/android/app/src/main/java/com/autocompletedropdownplayground/MainActivity.kt
+++ example/android/app/src/main/java/com/autocompletedropdownplayground/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.autocomletedropdownplayground
+package com.autocompletedropdownplayground
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "AutocomleteDropdownPlayground"
+  override fun getMainComponentName(): String = "AutocompleteDropdownPlayground"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- example/ios/AutocompleteDropdownPlayground/AppDelegate.swift
+++ example/ios/AutocompleteDropdownPlayground/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "AutocomleteDropdownPlayground",
+      withModuleName: "AutocompleteDropdownPlayground",
       in: window,
       launchOptions: launchOptions
     )
